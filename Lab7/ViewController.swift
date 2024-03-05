e6509147036f5c9388826bbb7f1f2a5c240ff0a8
Daniil Paskal <pask.daniil@gmail.com>
2024-03-05 10:08:04 -0500

    Initial Commit

---
+++ Lab7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab7
+//
+//  Created by user237236 on 3/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
